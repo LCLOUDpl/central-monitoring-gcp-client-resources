dafc801b3b5f32f13464065945291903201b7f61
Tomasz Skibinski <tomasz.skibinski@lcloud.pl>
2023-09-20 16:25:08 +0200

    chore: extend available outputs
    refs #217377

--- outputs.tf
+++ outputs.tf
@@ -2,14 +2,14 @@
  * Configuration file with outputs.
  */
 
-output "monitoring_topic_id" {
-  description = "ID of the monitoring Pub/Sub topic"
-  value       = google_pubsub_topic.lcloud_support.id
+output "monitoring_notification_channel_id" {
+  description = "ID of the monitoring notification channel"
+  value       = google_monitoring_notification_channel.lcloud_support.id
 }
 
-output "monitoring_topic_name" {
-  description = "Name of the monitoring Pub/Sub topic"
-  value       = google_pubsub_topic.lcloud_support.name
+output "monitoring_notification_channel_name" {
+  description = "Name of the monitoring notification channel"
+  value       = google_monitoring_notification_channel.lcloud_support.name
 }
 
 output "monitoring_subscription_id" {
@@ -20,4 +20,14 @@ output "monitoring_subscription_id" {
 output "monitoring_subscription_name" {
   description = "Name of the monitoring subscription system"
   value       = google_pubsub_subscription.lcloud_support.name
+}
+
+output "monitoring_topic_id" {
+  description = "ID of the monitoring Pub/Sub topic"
+  value       = google_pubsub_topic.lcloud_support.id
+}
+
+output "monitoring_topic_name" {
+  description = "Name of the monitoring Pub/Sub topic"
+  value       = google_pubsub_topic.lcloud_support.name
 }
